8bed0af38636d740717e62d0858c306b558afedf
Michael Bond <MichaelRBond@users.noreply.github.com>
2017-11-28 21:54:21 -0500

    Add mx records for Zoho

--- src/infrastructure.tf
+++ src/infrastructure.tf
@@ -45,6 +45,42 @@ resource "digitalocean_record" "mn-www" {
   value  = "@"
 }
 
+resource "digitalocean_record" "mn-zoho-verify" {
+  domain = "${digitalocean_domain.morgantown-ninja.name}"
+  type   = "CNAME"
+  name   = "zb15115206"
+  value  = "zmverify.zoho.com."
+}
+
+resource "digitalocean_record" "mn-mx-1" {
+  domain   = "${digitalocean_domain.morgantown-ninja.name}"
+  type     = "MX"
+  name     = "@"
+  value    = "mx.zoho.com."
+  priority = "10"
+}
+
+resource "digitalocean_record" "mn-mx-2" {
+  domain   = "${digitalocean_domain.morgantown-ninja.name}"
+  type     = "MX"
+  name     = "@"
+  value    = "mx2.zoho.com."
+  priority = "20"
+}
+
+resource "digitalocean_record" "mn-txt-spf" {
+  domain   = "${digitalocean_domain.morgantown-ninja.name}"
+  type     = "TXT"
+  name     = "@"
+  value    = "v=spf1 include:zoho.com ~all"
+}
+
+resource "digitalocean_record" "mn-txt-dkim" {
+  domain   = "${digitalocean_domain.morgantown-ninja.name}"
+  type     = "TXT"
+  name     = "default._domainkey"
+  value    = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCLoMjPHZMuXaxiW250MpOSobNHaWVBCOz7Eg/5hgl+wIdPWsm6tvXhWU2gYjb7V5r+ELtfAb0SiIeGewh5mTx2R2CjUMC4lyRyekTJ0oSopZwQ0qSA2AZJXzUFAJcwGLUM1ld3jOBks+BwMc1De5+EdvTHE6J4KYAhM0ki0kEW3QIDAQAB"
+}
 
 # The-Forgotten.Org
 resource "digitalocean_domain" "the-forgotten-org" {
@@ -130,10 +166,10 @@ resource "digitalocean_domain" "trumpocalyp-se" {
 }
 
 # MX -- This needs switched to mail.the-forgotten.org when it moves over.
-resource "digitalocean_record" "tfo-mx" {
-  domain   = "${digitalocean_domain.morgantown-ninja.name}"
-  type     = "MX"
-  name     = "mail"
-  value    = "@"
-  priority = "1"
-}
+# resource "digitalocean_record" "tfo-mx" {
+#   domain   = "${digitalocean_domain.morgantown-ninja.name}"
+#   type     = "MX"
+#   name     = "mail"
+#   value    = "@"
+#   priority = "1"
+# }
